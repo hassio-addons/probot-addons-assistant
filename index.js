3010ad3ec69c61cb04f7f1fc10b035d877fe9997
Franck Nijhof <frenck@geekchimp.com>
2018-05-04 21:59:57 +0200

    :hammer: Updates bot to use documented method

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = probotPlugin
-
-const wipBot=require('wip-bot');
-const firstTimersBot=require('first-timers-bot');
-
-function probotPlugin(robot) {
-    wipBot(robot)
-    firstTimersBot(robot)
-}
